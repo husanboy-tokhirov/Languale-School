e0a7bfe846dcd9ce1f9a558a300d484f2fd81b54
husanboy-tokhirov <husanboytoxirov27032004@gmail.com>
2025-01-26 13:19:54 +0500

    finished.end

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var elMenu = document.querySelector(".menu")
+
+function handleMenuBtnClick(){
+    elMenu.classList.add("right-0")
+}
+function handleCloseMenu(){
+    elMenu.classList.remove("right-0")
+}
